b36cb12b68f5fb59f4e69cf32888c28f2928838f
dell <vasan2001@gmail.com>
2023-09-27 17:07:07 -0500

    added favorite movie(s)

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,15 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+      :release_date => '13-Aug-2021'},
+    {:title => 'Spider Man: No Way Home', :rating => 'PG-13',
+      :release_date => '17-Dec-2021'},
+    {:title => 'Jailer', :rating => 'PG-13',
+      :release_date => '09-Aug-2023'},
+    {:title => 'RRR', :rating => 'PG-13',
+      :release_date => '24-Mar-2022'},
+    {:title => 'Pursuit of Happyness', :rating => 'PG-13',
+      :release_date => '15-Dec-2006'}
   ]
   
   more_movies.each do |movie|
